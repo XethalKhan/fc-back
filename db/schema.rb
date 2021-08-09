de7b45e478bfce601d8277ba8e1d98fb3374c406
master <master@localhost.localdomain>
2021-08-09 18:38:22 +0200

    v1.0.3 - Added docker-compose.yml file
    Added docker compose file and modified database config file accordingly

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_08_04_164259) do
 
-  create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_admin_comments", charset: "utf8mb4", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_164259) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
-  create_table "admin_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "admin_users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_164259) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "login_sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "login_sessions", charset: "utf8mb4", force: :cascade do |t|
     t.string "token"
     t.datetime "start"
     t.datetime "end"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_164259) do
     t.index ["user_id"], name: "index_login_sessions_on_user_id"
   end
 
-  create_table "time_sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "time_sessions", charset: "utf8mb4", force: :cascade do |t|
     t.datetime "start"
     t.datetime "end"
     t.datetime "created_at", precision: 6, null: false
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_164259) do
     t.index ["user_id"], name: "index_time_sessions_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "full_name"
     t.string "email"
     t.string "password"
